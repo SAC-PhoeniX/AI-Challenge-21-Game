2bb16de81e71ad9372d9a8566e0d7c483afc5f1e
Cihan Bosnalı <cihanbosna@hotmail.com>
2021-03-21 17:53:32 +0300

    add bullet movement

--- TankA.gd
+++ TankA.gd
@@ -1,10 +1,16 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var speed = 100
 export (float) var rotation_speed = 2.5
 
 var velocity = Vector2()
 var rotation_dir = 0
+
+export var bullet_speed = 400
+export var fire_delay = 1.2
+
+var projectile = load("res://Projectile.tscn")
+var can_fire = true
 
 func get_input():
 	rotation_dir = 0
@@ -14,11 +20,23 @@ func get_input():
 	if Input.is_action_pressed('ui_left'):
 		rotation_dir -= 1
 	if Input.is_action_pressed('ui_down'):
-		velocity = Vector2(0, speed).rotated(rotation)
+		velocity = Vector2(-speed, 0).rotated(rotation)
 	if Input.is_action_pressed('ui_up'):
-		velocity = Vector2(0, -speed).rotated(rotation)
-
+		velocity = Vector2(speed, 0).rotated(rotation)
+	if Input.is_action_pressed("ui_select") and can_fire:
+		shoot()
+		can_fire = false
+		yield(get_tree().create_timer(fire_delay), "timeout")
+		can_fire = true
+		
 func _process(delta):
 	get_input()
 	rotation += rotation_dir * rotation_speed * delta
 	velocity = move_and_slide(velocity)
+
+func shoot():
+	var bullet = projectile.instance()
+	bullet.position = get_child(2).get_global_position()
+	bullet.rotation_degrees = rotation_degrees
+	bullet.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
+	get_tree().get_root().add_child(bullet)
