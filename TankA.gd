6553f426d13003045fd117a4f2bad081a28be773
Cihan Bosnalı <cihanbosna@hotmail.com>
2021-03-21 16:18:23 +0300

    add movement tank A

--- TankA.gd
+++ TankA.gd
@@ -1,25 +1,24 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 180
-const ACCELERATION = 200
-const FRICTION = 400
-var velocity = Vector2.ZERO
+export (int) var speed = 200
+export (float) var rotation_speed = 2.5
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var velocity = Vector2()
+var rotation_dir = 0
 
-
+func get_input():
+	rotation_dir = 0
+	velocity = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		rotation_dir += 1
+	if Input.is_action_pressed('ui_left'):
+		rotation_dir -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity = Vector2(0, speed).rotated(rotation)
+	if Input.is_action_pressed('ui_up'):
+		velocity = Vector2(0, -speed).rotated(rotation)
 
 func _process(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized()
-	
-	if input_vector != Vector2.ZERO: 
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-
-	velocity =  move_and_slide(velocity)
+	get_input()
+	rotation += rotation_dir * rotation_speed * delta
+	velocity = move_and_slide(velocity)
