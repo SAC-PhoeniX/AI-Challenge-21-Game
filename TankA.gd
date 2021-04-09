3aedfc46ef2b2d6e88a1cc5accdff4e58efd2181
Cihan Bosnalı <cihanbosna@hotmail.com>
2021-04-09 12:25:57 +0300

    Data collection play

--- TankA.gd
+++ TankA.gd
@@ -16,40 +16,29 @@ var pos2_y = 620
 var rng = RandomNumberGenerator.new()
 
 var score_time_start
-var motor_input = 0
-var rotation_input = 0 
-var fire_input = 0
 
 func _ready():
 	score_time_start = OS.get_unix_time()
-	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
 
-func _on_request_completed(result, response_code, headers, body):
-	
-	var json = JSON.parse(body.get_string_from_utf8())
-	rotation_input = json.result["r"]
-	motor_input = json.result["m"]
-	fire_input = json.result["f"]
 
 func get_input():
 	rotation_dir = 0
 	velocity = Vector2()
-	if rotation_input == 1:
+	if Input.is_action_pressed('ui_right'):
 		rotation_dir += 1
-	if rotation_input == -1:
+	if Input.is_action_pressed('ui_left'):
 		rotation_dir -= 1
-	if motor_input == -1:
-		velocity = Vector2(-speed, 0).rotated(rotation)
-	if motor_input == 1:
+	if Input.is_action_pressed('ui_up'):
 		velocity = Vector2(speed, 0).rotated(rotation)
-	if (fire_input == 1) and Global.TeamACanFire:
+	if Input.is_action_pressed('ui_down'):
+		velocity = Vector2(-speed, 0).rotated(rotation)
+	if (Input.is_action_pressed("ui_select")) and Global.TeamACanFire:
 		shoot()
 		Global.TeamACanFire = false
 		yield(get_tree().create_timer(fire_delay), "timeout")
 		Global.TeamACanFire = true
 		
 func _process(delta):
-	$HTTPRequest.request("http://127.0.0.1:4000")
 	get_input()
 	rotation += rotation_dir * rotation_speed * delta
 	velocity = move_and_slide(velocity, Vector2.UP, false, 4, PI/4, false)
--- TankB.gd
+++ TankB.gd
@@ -22,33 +22,26 @@ var fire_input = 0
 
 func _ready():
 	score_time_start = OS.get_unix_time()
-	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
 
-func _on_request_completed(result, response_code, headers, body):
-	var json = JSON.parse(body.get_string_from_utf8())
-	rotation_input = json.result["r"]
-	motor_input = json.result["m"]
-	fire_input = json.result["f"]
 	
 func get_input():
 	rotation_dir = 0
 	velocity = Vector2()
-	if rotation_input == 1:
+	if Input.is_action_pressed('d'):
 		rotation_dir += 1
-	if rotation_input == -1:
+	if Input.is_action_pressed('a'):
 		rotation_dir -= 1
-	if motor_input == -1:
-		velocity = Vector2(-speed, 0).rotated(rotation)
-	if motor_input == 1:
+	if Input.is_action_pressed('w'):
 		velocity = Vector2(speed, 0).rotated(rotation)
-	if (fire_input == 1) and Global.TeamBCanFire:
+	if Input.is_action_pressed('s'):
+		velocity = Vector2(-speed, 0).rotated(rotation)
+	if (Input.is_action_pressed("q")) and Global.TeamBCanFire:
 		shoot()
 		Global.TeamBCanFire = false
 		yield(get_tree().create_timer(fire_delay), "timeout")
 		Global.TeamBCanFire = true
 		
 func _process(delta):
-	$HTTPRequest.request("http://127.0.0.1:5000")
 	get_input()
 	rotation += rotation_dir * rotation_speed * delta
 	velocity = move_and_slide(velocity, Vector2.UP, false, 4, PI/4, false)
