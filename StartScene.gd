714a99eb5c49b787f1c583b298efcb38b3d9f226
Cihan Bosnalı <cihanbosna@hotmail.com>
2021-05-03 17:31:34 +0300

    Can change maps in start page

--- StartScene.gd
+++ StartScene.gd
@@ -7,6 +7,5 @@ func _process(delta):
 	if Input.is_action_pressed("ui_accept"):
 		Global.TeamAName = self.get_child(1).text
 		Global.TeamBName = self.get_child(2).text
-		Global.TeamANumber = self.get_child(3).text
-		Global.TeamBNumber = self.get_child(4).text
-		get_tree().change_scene("res://MapC.tscn")
+		print(self.get_child(3).text[0])
+		get_tree().change_scene("res://Map"+self.get_child(3).text[1]+".tscn")
