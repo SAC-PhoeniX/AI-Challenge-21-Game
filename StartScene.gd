8270eb218a8c6907a9c05992956ef3b30a3250c5
Cihan Bosnalı <cihanbosna@hotmail.com>
2021-05-03 21:55:12 +0300

    fix lowercase error

--- StartScene.gd
+++ StartScene.gd
@@ -8,4 +8,4 @@ func _process(delta):
 		Global.TeamAName = self.get_child(1).text
 		Global.TeamBName = self.get_child(2).text
 		print(self.get_child(3).text[0])
-		get_tree().change_scene("res://Map"+self.get_child(3).text[1]+".tscn")
+		get_tree().change_scene("res://Map"+self.get_child(3).text[1].to_upper()+".tscn")
